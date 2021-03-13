fb60772d8ca52d0833f775b13f48a8b3c0f5ce09
TODDDD <69815276+Whyamr501@users.noreply.github.com>
2021-03-13 23:33:16 +0700

    lancelot is lancelot

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage_lava.mk
+   $(LOCAL_DIR)/nad_lancelot.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lava-user \
-    lineage_lava-userdebug \
-    lineage_lava-eng
+    nad_lancelot-user \
+    nad_lancelot-userdebug \
+    nad_lancelot-eng
