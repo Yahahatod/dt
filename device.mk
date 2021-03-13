7aab6301ec55f3619328a0df46d3ecfda854d6d7
TODDDD <69815276+Whyamr501@users.noreply.github.com>
2021-03-14 00:34:28 +0700

    up

--- device.mk
+++ device.mk
@@ -84,6 +84,34 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libsuspend \
     android.hardware.health@2.0
+    
+# RcsService
+
+PRODUCT_PACKAGES += \
+
+    RcsService
+
+    
+
+# WiFi
+
+PRODUCT_PACKAGES += \
+
+    WifiOverlay \
+
+    TetheringConfigOverlay
+
+# [DNM] Temp permissions
+
+PRODUCT_COPY_FILES += \
+
+    $(LOCAL_PATH)/permissions/xyz.extras.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/xyz.extras.xml \
+
+    $(LOCAL_PATH)/permissions/xyz.extras.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/xyz.extras.xml \
+
+    $(LOCAL_PATH)/permissions/xyz.extras.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/xyz.extras.xml
+
+
 
 # IMS
 PRODUCT_BOOT_JARS += \
